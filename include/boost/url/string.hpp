6c2aedf58f335c97bbed18cfb17695bc064a66a7
Alexandre Bossard <alexandre.bossard@tanker.io>
2021-09-16 18:24:15 +0200

    [skip ci] wip

--- include/boost/url/string.hpp
+++ include/boost/url/string.hpp
@@ -11,23 +11,40 @@
 #define BOOST_URL_STRING_HPP
 
 #include <boost/url/detail/config.hpp>
+#ifndef BOOST_URL_USE_STD_STRING_VIEW
 #include <boost/utility/string_view.hpp>
+#else
+#include <string_view>
+#endif
+
 #include <string>
 
 namespace boost {
 namespace urls {
 
 /** The type of string_view used by the library
-*/
+ */
+#ifndef BOOST_URL_USE_STD_STRING_VIEW
 using string_view = boost::string_view;
+#else
+using string_view = std::string_view;
+#endif
 
 /// The string alias template return type for allocating member functions.
-template<class Allocator>
-using string_type =
-    std::basic_string<char,
-        std::char_traits<char>, Allocator>;
+template <class Allocator>
+using string_type = std::basic_string<char, std::char_traits<char>, Allocator>;
 
-} // urls
-} // boost
+// cpp17 does not have std::string_view::starts_with
+constexpr bool starts_with(string_view v, string_view::value_type c) {
+  return !v.empty() && string_view::traits_type::eq(c, v.front());
+}
+
+constexpr bool starts_with(string_view v, string_view s) {
+  return v.size() >= s.size() &&
+         string_view::traits_type::compare(v.data(), s.data(), s.size());
+}
+
+} // namespace urls
+} // namespace boost
 
 #endif
